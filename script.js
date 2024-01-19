3a423dcc615ac4e9d9bef2a2f7eb913ccf581dc6
Avinash <134164909+AvinashP-1611@users.noreply.github.com>
2024-01-19 15:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species=species;
+	}
+	get getSpecies(){
+		return this._species;
+	}
+	makeSound(){
+		console.log("Generic animal sound")
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
